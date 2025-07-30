4546f141dc3b340fd805ddd6712d73029ece8407
Lucas Peres <lucasperes@biud.com.br>
2025-07-30 16:08:40 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import serviceBell from "./assets/elegant-hotel-service-bell-96x96.png";
+import wppIcon from "./assets/whatsapp-icon.svg";
+import arrowRight from "./assets/arrow-right.svg";
+import outletPremium from "./assets/outlet-premium.png";
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="top-strip" />
+
+      <main>
+        <div className="icon-container">
+          <img src={serviceBell} alt="Campainha de balcão de hotel" />
+        </div>
+
+        <div className="text">
+          <p className="title">
+            Seu concierge pessoal no Outlet Premium na palma da mão.
+          </p>
+          <p className="subtitle">
+            Informe seu WhatsApp para falar com o concierge e receber descontos
+            exclusivos enquanto estiver no shopping.
+          </p>
+        </div>
+
+        <div className="input-container">
+          <label htmlFor="wpp-input" className="number-label">
+            <p>Seu WhatsApp</p>
+            <div className="input-icon">
+              <img src={wppIcon} width={24} alt="Ícone do WhatsApp" />
+              <input
+                id="wpp-input"
+                placeholder="(99) 99999-9999"
+                maxLength={15}
+              />
+            </div>
+          </label>
+
+          <div className="checkbox-container">
+            <input type="checkbox" id="terms" />
+            <label htmlFor="terms" className="checkbox-label">
+              Concordo com os{" "}
+              <a href="" target="_blank">
+                Termos de uso
+              </a>{" "}
+              e a{" "}
+              <a href="" target="_blank">
+                Política de Privacidade
+              </a>
+              .
+            </label>
+          </div>
+        </div>
+
+        <button type="button" className="start-btn">
+          <span>Começar</span>
+          <img src={arrowRight} width={24} alt="Ícone do WhatsApp" />
+        </button>
+      </main>
+
+      <img
+        src={outletPremium}
+        alt="Logo do shopping"
+        className="shopping-logo"
+      />
+    </div>
+  );
+}
